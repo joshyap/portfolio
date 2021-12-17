14da070634596a86e887758f8ad72f9b38cfeefc
Josh Yap <joshua.c.yap@gmail.com>
2021-12-17 00:23:40 -0600

    layout test

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// let evenDescriptions = document.querySelectorAll('.description');
+// let evenImages = document.querySelectorAll('.portfolioImage');
+
+// // for (var i=0; len=evenDescriptions.length; i++) {
+// for (var i=0; i<5; i++) {
+//     if (i%2 === 0) {
+//         evenDescriptions[i].className = 'rightColumn';
+//         evenImages[i].className = 'leftColumn';
+//     }
+// }
